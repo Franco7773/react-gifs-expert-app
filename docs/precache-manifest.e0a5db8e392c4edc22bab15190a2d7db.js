220460a25464093a7d2abb5e1224b76f73b942a8
Gianfranco D'Onofrio <gian7773@gmail.com>
2020-07-02 12:36:51 -0400

    Docs

---
+++ docs/precache-manifest.e0a5db8e392c4edc22bab15190a2d7db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4deecae0dac44b0c07b73f1fc75b8970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568f2e4970332c08c508",
+    "url": "/static/css/main.d80ffa15.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568f2e4970332c08c508",
+    "url": "/static/js/main.3aed1960.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
